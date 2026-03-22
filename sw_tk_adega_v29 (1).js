ffa007c08c227f8cb41adce518aae567016c7503
lucasgabriel3120-crypto <lucasgabriel3120@gmail.com>
2026-03-22 12:14:54 -0300

    Add files via upload

---
+++ sw_tk_adega_v29 (1).js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "tk-adega-v30-cache";
+const URLS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "https://cdn.jsdelivr.net/npm/chart.js",
+  "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js",
+  "https://www.gstatic.com/firebasejs/10.12.2/firebase-database-compat.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+      return response;
+    }))
+  );
+});
